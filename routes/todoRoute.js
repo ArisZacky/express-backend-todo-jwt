593704c467ff1e7dbe3118bc60eb37f9b8c26195
unknown <rizkyariszacky@gmail.com>
2024-02-27 17:28:08 +0800

    fixing bug, (now it invalid token error 500)

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -3,10 +3,10 @@ const TodoController = require("../controllers/todoController");
 const auth = require("../middlewares/auth");
 const router = Router();
 
-router.get('/todo', TodoController.all)
-router.post("/todo/add", TodoController.store);
-router.get("/todo/:id", TodoController.detail);
-router.put("/todo/update/:id", TodoController.update);
-router.delete("/todo/delete/:id", TodoController.delete);
+router.get('/todo', auth, TodoController.all)
+router.post("/todo/add", auth, TodoController.store);
+router.get("/todo/:id", auth, TodoController.detail);
+router.put("/todo/update/:id", auth, TodoController.update);
+router.delete("/todo/delete/:id", auth, TodoController.delete);
 
 module.exports = router;
