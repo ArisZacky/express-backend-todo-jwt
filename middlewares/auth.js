7c37db74f16ee880473bb0dbccea863f6081304f
unknown <rizkyariszacky@gmail.com>
2024-02-28 16:09:13 +0800

    fixing authentication token (already fixed and can be used now for todo and auth)

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
     if (req.headers.authorization) {
-        let token = req.headers.authorization;
+        let token = req.headers.authorization.replace("Bearer ", "");
+        console.log(token, process.env.TOKEN_SECRET)
         req.loggedUser = jwt.verify(token, process.env.TOKEN_SECRET);
         next();
       } else {
